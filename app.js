e1070ccbf50109150747127804437d8f854d426a
Hashem <hashemrashed357@gmail.com>
2026-05-09 19:07:18 +0300

    fix search

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ let state = {
   countryLabel: ''
 };
 
+
 /* ── DOM Refs ── */
 const cityInput      = document.getElementById('city-input');
 const searchBtn      = document.getElementById('search-btn');
